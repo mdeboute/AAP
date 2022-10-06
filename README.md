86bc8e0773bbb7d731375a1d24f60ed0b9303182
mdeboute <martin.deboute@gmail.com>
2022-10-06 10:03:49 +0200

    big refactoring

--- README.md
+++ README.md
@@ -21,12 +21,8 @@ In the build directory:
 ./a.out
 ```
 
-### First report, **deadline: Friday, September 30th**
+### Second report, **deadline: Friday, October 28th**
 
 Write a small report (PDF file) explaining:
 
-The construction of the graph: what are the vertices? the edges?
-The formal statement of the problem: what is a typical instance (input, question)? -> We have to define all the notions used (for example, what is represented by a vertex?)
-Explain and justify our choices (algorithmic and programming) in a few words! (We can insert some code if necessary).
-How do we go from a solution of our graph problem to the coordinates (of the image) where to place the firefighter actions?
-Any information that we consider useful is welcome.
+BrutForce + Greedy algorithms.
--- src/exec/mip.cpp
+++ src/exec/mip.cpp
@@ -1,5 +1,4 @@
 #include "file_io.hpp"
-#include "filesystem.hpp"
 #include "mip_formulation.hpp"
 #include "utils.hpp"
 #include <string>
@@ -9,11 +8,15 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    const string root_dir = fs::current_path().parent_path().string();
-    const string data_dir = root_dir + "/data";
+    if (argc != 2)
+    {
+        cout << "Usage: ./mip.out <input_dir>" << endl;
+        return 1;
+    }
 
-    string map_file = data_dir + "/map.ppm";
-    string config_file = data_dir + "/config.txt";
+    const string data_dir = argv[1];
+    const string map_file = data_dir + "/map.ppm";
+    const string config_file = data_dir + "/config.txt";
 
     vector<float> config = parseConfig(config_file);
     vector<vector<Color>> map = parseMap(map_file);
@@ -28,8 +31,9 @@ int main(int argc, char *argv[])
 
     map = solve(map, config);
 
-    const string result_dir = root_dir + "/results";
-    string result_map_file = result_dir + "/result.ppm";
+    const string result_dir = "../solutions";
+    const string result_map_file = result_dir + "/result.ppm";
+    cout << "Writing result to " << result_map_file << endl;
     writeMap(result_map_file, map);
 
     display_map(map);
