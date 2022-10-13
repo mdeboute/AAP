546c0395978f82518991742c25eb5d429970d4d3
mdeboute <martin.deboute@gmail.com>
2022-10-13 10:01:48 +0200

    done with the partition

---
+++ include/brute_force.hpp
@@ -0,0 +1,9 @@
+#ifndef BRUTE_FORCE_HPP
+#define BRUTE_FORCE_HPP
+
+#include <vector>
+#include "Graph/FighterVertex.hpp"
+
+std::vector<std::vector<FighterVertex>> findPartitions(std::vector<FighterVertex> fighterList);
+
+#endif
---
+++ src/brute_force.cpp
@@ -0,0 +1,9 @@
+#include "brute_force.hpp"
+#include <vector>
+#include "Graph/FighterVertex.hpp"
+
+std::vector<std::vector<FighterVertex>> findPartitions(std::vector<FighterVertex> fighterList)
+{
+    std::vector<std::vector<FighterVertex>> partitions;
+    return partitions;
+}
