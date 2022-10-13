e2897408e1c815c0eefed230600a9c334eeb6559
mdeboute <martin.deboute@gmail.com>
2022-10-13 10:01:53 +0200

    done

--- include/brute_force.hpp
+++ include/brute_force.hpp
@@ -3,7 +3,9 @@
 
 #include <vector>
 #include "Graph/FighterVertex.hpp"
+#include "Graph/FireVertex.hpp"
 
 std::vector<std::vector<FighterVertex>> findPartitions(std::vector<FighterVertex> fighterList);
+bool checker(std::vector<std::vector<FighterVertex>> fightersPartition, std::vector<FireVertex> fireList);
 
 #endif
--- src/brute_force.cpp
+++ src/brute_force.cpp
@@ -1,9 +1,24 @@
 #include "brute_force.hpp"
 #include <vector>
 #include "Graph/FighterVertex.hpp"
+#include <cmath>
 
 std::vector<std::vector<FighterVertex>> findPartitions(std::vector<FighterVertex> fighterList)
 {
+    int n = fighterList.size();
     std::vector<std::vector<FighterVertex>> partitions;
+
+    for (int i = 0; i < pow(2, n); i++)
+    {
+        std::vector<FighterVertex> partition;
+        for (int j = 0; j < n; j++)
+        {
+            if (i & (1 << j))
+            {
+                partition.push_back(fighterList[j]);
+            }
+        }
+        partitions.push_back(partition);
+    }
     return partitions;
 }
