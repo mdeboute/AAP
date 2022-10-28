5cfd948dde4c986a5d72e0d7cadc1b86fefca4b4
mdeboute <martin.deboute@gmail.com>
2022-10-28 16:04:48 +0200

    clean the code of the exec

--- src/exec/test.cpp
+++ src/exec/test.cpp
@@ -9,12 +9,37 @@
 
 using namespace std;
 
+void display_usage()
+{
+    cout << "Usage: ./test <instance_directory> <algorithm>" << endl;
+    cout << "Where <algorithm> is one of:" << endl;
+    cout << "-b or --bruteforce" << endl;
+    cout << "-g or --greedy" << endl;
+    cout << "-m or --mip" << endl;
+}
+
+void display_solution(const vector<FighterVertex> solution)
+{
+    for (FighterVertex fighter : solution)
+    {
+        cout << "We place a fighter at position (" << fighter.getPos().getX() << ", " << fighter.getPos().getY() << ")" << endl;
+    }
+}
+
+void display_data(vector<float> config, vector<vector<Color>> map)
+{
+    cout << "Number of angles: " << config[0] << endl;
+    cout << "Furnace radius: " << config[1] << endl;
+    cout << "Radius of action of a firefighter: " << config[2] << endl;
+    cout << "Map size: " << map.size() << "x" << map[0].size() << endl;
+    cout << endl;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
-        cout << "Usage: ./test.out <instance_dir> <algo>" << endl;
-        cout << "Algo: -b for brute force, -g for greedy" << endl;
+        display_usage();
         return 1;
     }
 
@@ -25,36 +50,25 @@ int main(int argc, char *argv[])
     vector<float> config = parse_config(config_file);
     vector<vector<Color>> map = parse_map(map_file);
 
-    cout << "Number of angles: " << config[0] << endl;
-    cout << "Furnace radius: " << config[1] << endl;
-    cout << "Radius of action of a firefighter: " << config[2] << endl;
-    cout << "Map size: " << map.size() << "x" << map[0].size() << endl;
-    cout << endl;
+    display_data(config, map);
 
     Graph graph = calculate_graph_data(map, config, true, true);
 
     vector<string> splitString = split_string(data_dir, "/");
     if (splitString[splitString.size() - 1].empty())
-        splitString.erase(splitString.end()-1);
+        splitString.erase(splitString.end() - 1);
     const string result_file = "../solution/result_" + splitString[splitString.size() - 1] + ".ppm";
 
     if (strcmp(argv[2], "-b") == 0)
     {
         vector<FighterVertex> bestTeam = bruteforce_solve(graph);
-        for (FighterVertex fighter : bestTeam)
-        {
-            cout << "We place a fighter at position (" << fighter.getPos().getX() << ", " << fighter.getPos().getY() << ")" << endl;
-        }
-        cout << "\nWriting result to " << result_file << endl;
+        display_solution(bestTeam);
         write_solution(result_file, map, config, bestTeam);
     }
     else if (strcmp(argv[2], "-g") == 0)
     {
         vector<FighterVertex> bestTeam = greedy_solve(graph);
-        for (FighterVertex fighter : bestTeam)
-        {
-            cout << "We place a fighter at position (" << fighter.getPos().getX() << ", " << fighter.getPos().getY() << ")" << endl;
-        }
+        display_solution(bestTeam);
         cout << "\nWriting result to " << result_file << endl;
         write_solution(result_file, map, config, bestTeam);
     }
