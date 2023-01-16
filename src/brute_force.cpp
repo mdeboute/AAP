ef58dbda2c823d1cd89b4345b6a7759ad31eb4a6
mdeboute <martin.deboute@gmail.com>
2023-01-16 15:27:57 +0100

    last commit i guess?

--- src/brute_force.cpp
+++ src/brute_force.cpp
@@ -22,166 +22,123 @@ int compute_lower_bound(const std::vector<FighterVertex> &fighterList, const std
     return ceil((double)fireList.size() / max);
 }
 
-//std::vector<FighterVertex> bruteforce_solve(const Graph &graph)
-//{
-//    std::vector<FighterVertex> fighters = graph.getFigtherVertexList();
-//    std::vector<FireVertex> fires = graph.getFireVertexList();
-//
-//    auto startingTime = std::chrono::steady_clock::now();
-//
-//    int n = fighters.size();
-//
-//    std::cout << "Number of potential fighters: " << n << std::endl;
-//
-//    int upperBound = n;
-//    int lowerBound = compute_lower_bound(fighters, fires);
-//
-//    // sort the fighters list by the number of fires they can stop (we earn ~10% of the time)
-//    std::sort(fighters.begin(), fighters.end(), [](FighterVertex &a, FighterVertex &b)
-//              { return a.getNbFireCovered() > b.getNbFireCovered(); });
-//
-//    std::vector<FighterVertex> bestSolution;
-//    int bestSize = upperBound;
-//
-//    for (int i = 0; i < std::pow(2, n); i++)
-//    {
-//        std::vector<FighterVertex> team;
-//
-//        // we want the team of size at most upperBound and at least lowerBound that are feasible (using the checker)
-//        if (std::bitset<32>(i).count() <= upperBound && std::bitset<32>(i).count() >= lowerBound)
-//        {
-//            for (int j = 0; j < n; j++)
-//            {
-//                if (i & (1 << j))
-//                {
-//                    team.push_back(fighters[j]);
-//                }
-//            }
-//            if (check_feasibility(team, fires) && team.size() < bestSize)
-//            {
-//                bestSolution = team;
-//                bestSize = team.size();
-//            }
-//        }
-//    }
-//
-//    std::chrono::duration<double> tt = std::chrono::steady_clock::now() - startingTime;
-//    std::cout << "Result: runtime = " << tt.count() << " sec; objective value = " << bestSize << std::endl;
-//    std::cout << std::endl;
-//    return bestSolution;
-//}
+std::vector<FighterVertex> bruteforce_solve(const Graph &graph)
+{
+    std::vector<FighterVertex> fighters = graph.getFigtherVertexList();
+    std::vector<FireVertex> fires = graph.getFireVertexList();
 
- //void next(std::vector<bool> &partition)
- //{
- //    int listSize = partition.size();
- //    for (int i = 1; i < listSize; ++i)
- //    {
- //        if (partition[listSize - 1 - i] == 1 && partition[listSize - i] == 0)
- //        {
- //            partition[listSize - 1 - i] = 0;
- //            partition[listSize - i] = 1;
- //            int tmp = 0;
- //            for (int j = listSize - i + 1; j < listSize; ++j)
- //            {
- //                if (partition[j] == 1)
- //                {
- //                    tmp++;
- //                    partition[j] = 0;
- //                    partition[listSize - i + tmp] = 1;
- //                }
- //            }
- //            break;
- //        }
- //    }
- //}
+    auto startingTime = std::chrono::steady_clock::now();
 
- //std::vector<std::vector<bool>> generate_partitions(int n, int k)
- //{
- //    std::vector<std::vector<bool>> partitions;
- //    std::vector<bool> partition(n, 0);
- //    for (int i = 0; i < k; ++i)
- //    {
- //        partition[i] = 1;
- //    }
- //    partitions.push_back(partition);
- //    double np = std::tgamma(n + 1) / (std::tgamma(k + 1) * std::tgamma(n - k + 1));
- //    std::cout << "np equals " << np << std::endl;
- //    for (int i = 0; i < np; ++i)
- //    {
- //        next(partition);
- //        partitions.push_back(partition);
- //    }
- //    return partitions;
- //}
+    int n = fighters.size();
 
+    std::cout << "Number of potential fighters: " << n << std::endl;
 
-void new_generate_partitions(int n, int k, int i, int nb_selected, std::vector<bool> & partition, std::vector<std::vector<bool>> & partitions)
- {
-     if (i + k - nb_selected > n)
-         return;
-     if (nb_selected == k || i == n) {
-         std::vector<bool> complete_partition = partition;
-         partitions.push_back(complete_partition);
-         return;
-     }
-     partition.push_back(1);
-     new_generate_partitions(n, k, i + 1, nb_selected + 1, partition, partitions);
-     partition.pop_back();
-     partition.push_back(0);
-     new_generate_partitions(n, k, i + 1, nb_selected, partition, partitions);
-     partition.pop_back();
-     return;
- }
+    int upperBound = n;
+    int lowerBound = compute_lower_bound(fighters, fires);
 
- std::vector<FighterVertex> better_bruteforce_solve(const Graph &graph)
- {
-     std::vector<FighterVertex> fighters = graph.getFigtherVertexList();
-     std::vector<FireVertex> fires = graph.getFireVertexList();
+    // sort the fighters list by the number of fires they can stop (we earn ~10% of the time)
+    std::sort(fighters.begin(), fighters.end(), [](FighterVertex &a, FighterVertex &b)
+              { return a.getNbFireCovered() > b.getNbFireCovered(); });
 
-     auto startingTime = std::chrono::steady_clock::now();
+    std::vector<FighterVertex> bestSolution;
+    int bestSize = upperBound;
 
-     int n = fighters.size();
+    for (int i = 0; i < std::pow(2, n); i++)
+    {
+        std::vector<FighterVertex> team;
 
-     std::cout << "Number of potential fighters: " << n << std::endl;
+        // we want the team of size at most upperBound and at least lowerBound that are feasible (using the checker)
+        if (std::bitset<32>(i).count() <= upperBound && std::bitset<32>(i).count() >= lowerBound)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i & (1 << j))
+                {
+                    team.push_back(fighters[j]);
+                }
+            }
+            if (check_feasibility(team, fires) && team.size() < bestSize)
+            {
+                bestSolution = team;
+                bestSize = team.size();
+            }
+        }
+    }
 
-     int upperBound = n;
-     int lowerBound = compute_lower_bound(fighters, fires);
+    std::chrono::duration<double> tt = std::chrono::steady_clock::now() - startingTime;
+    std::cout << "Result: runtime = " << tt.count() << " sec; objective value = " << bestSize << std::endl;
+    std::cout << std::endl;
+    return bestSolution;
+}
 
-     std::sort(fighters.begin(), fighters.end(), [](FighterVertex &a, FighterVertex &b)
-               { return a.getNbFireCovered() > b.getNbFireCovered(); });
+void generate_partitions(int n, int k, int i, int nb_selected, std::vector<bool> &partition, std::vector<std::vector<bool>> &partitions)
+{
+    if (i + k - nb_selected > n)
+        return;
+    if (nb_selected == k || i == n)
+    {
+        std::vector<bool> complete_partition = partition;
+        partitions.push_back(complete_partition);
+        return;
+    }
+    partition.push_back(1);
+    generate_partitions(n, k, i + 1, nb_selected + 1, partition, partitions);
+    partition.pop_back();
+    partition.push_back(0);
+    generate_partitions(n, k, i + 1, nb_selected, partition, partitions);
+    partition.pop_back();
+    return;
+}
 
-     std::vector<FighterVertex> bestSolution;
-     int solFound = 0;
+std::vector<FighterVertex> better_bruteforce_solve(const Graph &graph)
+{
+    std::vector<FighterVertex> fighters = graph.getFigtherVertexList();
+    std::vector<FireVertex> fires = graph.getFireVertexList();
 
-     while (solFound == 0)
-     {
-         std::vector<std::vector<bool>> partitions;
-         std::vector<bool> partition;
-         new_generate_partitions(n, lowerBound, 0, 0, partition, partitions);
-         // std::cout << "Bound at " << lowerBound << " with " << partitions.size() << " partitions" << std::endl;
+    auto startingTime = std::chrono::steady_clock::now();
 
-         for (std::vector<bool> partition : partitions)
-         {
-             std::vector<FighterVertex> potentialSol;
-             for (int i = 0; i < partition.size(); ++i)
-             {
-                 if (partition[i] == 1)
-                 {
-                     potentialSol.push_back(fighters[i]);
-                 }
-             }
-             if (check_feasibility(potentialSol, fires))
-             {
-                 std::chrono::duration<double> tt = std::chrono::steady_clock::now() - startingTime;
-                 std::cout << "Result: runtime = " << tt.count() << " sec; objective value = " << potentialSol.size() << std::endl;
-                 std ::cout << std::endl;
-                 return potentialSol;
-             }
-         }
+    int n = fighters.size();
 
-         lowerBound++;
-     }
-     std::cout << "No solution found!" << std::endl;
-     std::cout << std::endl;
-     return std::vector<FighterVertex>();
- }
+    std::cout << "Number of potential fighters: " << n << std::endl;
+
+    int upperBound = n;
+    int lowerBound = compute_lower_bound(fighters, fires);
+
+    std::sort(fighters.begin(), fighters.end(), [](FighterVertex &a, FighterVertex &b)
+              { return a.getNbFireCovered() > b.getNbFireCovered(); });
+
+    std::vector<FighterVertex> bestSolution;
+    int solFound = 0;
+
+    while (solFound == 0)
+    {
+        std::vector<std::vector<bool>> partitions;
+        std::vector<bool> partition;
+        generate_partitions(n, lowerBound, 0, 0, partition, partitions);
+        // std::cout << "Bound at " << lowerBound << " with " << partitions.size() << " partitions" << std::endl;
+
+        for (std::vector<bool> partition : partitions)
+        {
+            std::vector<FighterVertex> potentialSol;
+            for (int i = 0; i < partition.size(); ++i)
+            {
+                if (partition[i] == 1)
+                {
+                    potentialSol.push_back(fighters[i]);
+                }
+            }
+            if (check_feasibility(potentialSol, fires))
+            {
+                std::chrono::duration<double> tt = std::chrono::steady_clock::now() - startingTime;
+                std::cout << "Result: runtime = " << tt.count() << " sec; objective value = " << potentialSol.size() << std::endl;
+                std ::cout << std::endl;
+                return potentialSol;
+            }
+        }
+
+        lowerBound++;
+    }
+    std::cout << "No solution found!" << std::endl;
+    std::cout << std::endl;
+    return std::vector<FighterVertex>();
+}
